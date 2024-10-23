80b857b912150085b75e79ab06424fff437e564a
Antonia0703 <antoniaandreea5@gmail.com>
2024-10-23 09:35:44 +0200

    clean up javascript and replaced attribute name

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-
-
 const buttons = document.querySelectorAll('.buttons'); // Select all buttons with the class 'buttons'
-const outputParagraph = document.getElementById('output'); // Select the output paragraph
-const buttonClear = document.getElementById('Clear'); // Select the clear button
+const buttonClear = document.getElementById('clear'); // Select clear button
+const dynamicText = document.getElementById('dynamic-text'); // Select output of card
 
 
-function showText(buttonText) {
-    const dynamicText = document.getElementById('dynamicText'); // Display the clicked text from bottons in the dynamicText paragraph between the greeting and closing
-    dynamicText.innerHTML += buttonText + '<br/>';
+function addTextToWeddingCard(buttonText) {
+    dynamicText.innerHTML += buttonText + '<br/>'; // Add text to output of card 
 }
 
-document.querySelectorAll('.buttons').forEach(button => {
+buttons.forEach(button => {
     button.addEventListener('click', function () {
         const message = this.getAttribute('data-message'); // Get the message from the button's data-message attribute
-        showText(message);
+        addTextToWeddingCard(message);
     });
 });
 
-
-document.getElementById('clear').addEventListener('click', function () {
-    const dynamicText = document.getElementById('dynamicText'); // Display the clicked text from bottons in the dynamicText paragraph between the greeting and closing
-    dynamicText.textContent = '';
+buttonClear.addEventListener('click', function () {
+    dynamicText.textContent = ''; // Replace output with empty string
 });
 
 
